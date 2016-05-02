567ebbdccc9b25cc16c1db2d9bd7a672b2977294
labster <bslaabs@gmail.com>
2016-05-02 15:27:43 -0700

    Use class instead of functions for code reuse, fix case on js
    Wrong case prevented JS from loading from submodule in Linux

--- ScratchBlocks.php
+++ ScratchBlocks.php
@@ -17,51 +17,20 @@ if (!defined('MEDIAWIKI')) {
     die();
 }
 
+require_once __DIR__ . "/ScratchblockHooks.php";
 
 // Hooks
 
-$wgExtensionFunctions[] = 'sbSetup';
-$wgHooks['ParserFirstCallInit'][] = 'sbParserInit';
- 
-
-// Hook callback function into parser
-
-function sbParserInit (Parser $parser) {
-    // Register <scratchblocks> and <sb> tag
-    $parser->setHook('scratchblocks', 'sbRenderTag');
-	$parser->setHook('sb', 'sbRenderInlineTag');
-    return true;
-}
- 
-
-// Ouput HTML for <scratchblocks> tag
-
-function sbRenderTag ($input, array $args, Parser $parser, PPFrame $frame) {
-    return '<pre class="blocks">' . htmlspecialchars($input) . '</pre>';
-}
-
-// Output HTML for inline <sb> tag
-
-function sbRenderInlineTag ($input, array $args, Parser $parser, PPFrame $frame) {
-	//throw new Exception("what");
-    return '<code class="blocks">' . htmlspecialchars($input) . '</code>';
-}
-
-
-// Make wiki load resources
-
-function sbSetup () {
-    global $wgOut;
-    $wgOut->addModules('ext.scratchBlocks');
-}
+$wgExtensionFunctions[] = 'ScratchblockHook::sbSetup';
+$wgHooks['ParserFirstCallInit'][] = 'ScratchblockHook::sbParserInit';
 
 
 // Define resources
 
 $wgResourceModules['ext.scratchBlocks'] = array(
     'scripts' => array(
-        'ScratchBlocks/src/scratchblocks.js',
-		'scratchblocks/src/translations.js',
+        'scratchblocks/src/scratchblocks.js',
+        'scratchblocks/src/translations.js',
         'run_scratchblocks.js',
     ),
 
